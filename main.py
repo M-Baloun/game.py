016229956767161043db24f3ac60d5912c633162
miroslav.baloun <miroslav.baloun@orbiszlin.cz>
2025-01-13 15:24:43 +0100

    Ended the project.

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ def crossroad():
     print("1 - Tréninkové hriště")
     print("2 - Krčma")
     print("3 - Souboj")
+    print("4 - Konec hry")
 
     choice = input("Vyber z menu: ")
     if int(choice) == 1:
@@ -82,12 +83,24 @@ def crossroad():
     elif int(choice) == 2:
         tavern()
     elif int(choice) == 3:
-        print("Budeš bojovat")
+        real_fight()
+    elif int(choice) == 4:
+        print("Ukončil si hru. Ve tvé hře jsi dosáhl: ")
+        stats()
+        exit()
     else:
         crossroad()
 
+def fight(win_under):
+    random_chance = random.randint(1,100)
+
+    if random_chance < win_under:
+        return True
+    else:
+        return False
 
 def training_course():
+    global xp
     print("-------------------------------")
     print("   Jsi na Tréninkovém hřišti   ")
     print("-------------------------------")
@@ -96,12 +109,33 @@ def training_course():
     print("1 - Útok")
     print("2 - Obrana")
 
-    random_chance = random.randint(1, 100)
 
-    if random_chance < 50:
-        print("vyhral jsi")
+    if fight(50):
+        xp += 1
+        print("vyhrál jsi")
     else:
-        print("prohral jsi")
+        print("prohrál jsi")
+
+    crossroad()
+def real_fight():
+    global xp, hp, money
+    print("-------------------------------")
+    print("   Potkal jsi monstrum   ")
+    print("-------------------------------")
+
+    print("\nVyber si typ boje")
+    print("1 - Útok")
+    print("2 - Obrana")
+
+    if fight(random.randint(40,60)):
+        xp += random.randint(5,30)
+        money += random.randint(10,40)
+        print("vyhrál jsi")
+    else:
+        hp -= random.randint(5,20)
+        print("prohrál jsi")
+
+        crossroad()
 
 def main():
     welcome_screen()
